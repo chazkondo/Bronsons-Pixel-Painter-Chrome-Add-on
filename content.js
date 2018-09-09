41813684829ab7dabd36c5f580be52f54844d319
chaz kondo <38027158+demifire@users.noreply.github.com>
2018-09-09 08:43:38 -1000

    Update content.js

--- content.js
+++ content.js
@@ -1,18 +1,1 @@
-console.log('sup');
-
-// document.body.style.backgroundImage =  "url('Bronson.png')";
-// document.body.style.backgroundRepeat = "repeat";
-
-// const myImg = new Image;
-// myImg.src = 'Bronson.png'
-
-// document.body.style.backgroundImage = myImg;
-
-const annoyingContent = document.getElementById('sites-migration-promo');
-annoyingContent.style.display = 'none';
-
-const moreAnnoying = document.getElementById('sites-searchbox-search-button');
-moreAnnoying.style.color = '#444444';
-moreAnnoying.style.userSelect = '';
-moreAnnoying.innerHTML = "<span2>Search this site</span2>";
-
+console.log('content.js currently has no content')
